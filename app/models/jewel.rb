c8b44ff8ed2e66240a543df853027673ad83c765
jongwonIee <ljw3728@gmail.com>
2017-07-10 21:55:48 +0900

    init, deal, calculate

--- app/models/jewel.rb
+++ app/models/jewel.rb
@@ -9,11 +9,24 @@ class Jewel < ApplicationRecord
       gold: 4,
       silver: 4,
       red: 1,
-      black: 1,
+      black: 1
     )
   end
 
-  def self.deal(n)
-    jewels = Jewel.find(n)
+  def self.deal(id)
+    (1..25).to_a.sample(10).each do |n|
+      Jewel.calculate(id, n, '-')
+    end
+  end
+
+  def self.calculate(id, n, operator)
+    jewels = Jewel.find(id)
+    if n == 1 and operator == '+'
+      jewels.ruby += 1
+      jewels.save
+    elsif n == 1 and operator == '-'
+      jewels.ruby -= 1
+      jewels.save
+    end
   end
 end
